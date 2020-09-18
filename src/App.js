676e5c5b82fd4b0f14c1f0de28fabd90ddad35b8
Adriano Viana <adrianoviana87@gmail.com>
2020-09-18 14:42:52 -0300

    changed feat1

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Yeah 777</p>
+        <p>New feat 1</p>
         <a
           className="App-link"
           href="https://reactjs.org"
